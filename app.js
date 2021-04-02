c3341a3e6fc5f753aed6e1c859604321dcd373ae
SLISETTE127 <SLISETTE127@GMAIL.COM>
2021-04-01 20:15:10 -0500

    homework

---
+++ app.js
@@ -0,0 +1,41 @@
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+let arrayOfTodos = [
+  {
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 20,
+  "id": 2,
+  "title": "delectus aut autem",
+  "completed": false
+}]
+
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then( (response) => response.json())
+  .then( (json) => arrayOfTodos = json)
+}
+
+const logTodos = () => {
+  console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+//creat an element <li>
+//get parent element <ol<
+//
+}
+
+
+const populateTodos = () =>
+
+const populateTodos = () => {
+
+//for (let index = 0; index < arrayOfTodos[index];
+ // console.log('element:', element))
+
+}
